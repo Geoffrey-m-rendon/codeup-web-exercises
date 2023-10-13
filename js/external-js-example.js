6d1889b20376dcc8bc9cfb00f2ec9c96c47ebde3
Geoffrey Michael Rendon <geoffrey.m.rendon@gmail.com>
2023-10-13 12:03:50 -0500

    feat :completed the jds w / html lec

---
+++ js/external-js-example.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let favoriteColor ; string = "black";
+console.log(favoriteColor);
+
+// Alert - A pop-up message to the user from JavaScript
+Alert("Howdy from," + studentname);
+
+//Confirm - A pop-up message to the user from JavaScript, that has the ability to capture a boolean
+let confirmed ; boolean = confirm("We have lunch today at 12:30?");
+console.log("confirmed");
+
+//Prompt - A pop-up message to the user from JavaScript,
