31ab68436b2601e66acf7d6f320812b50b51b463
Geoffrey Michael Rendon <geoffrey.m.rendon@gmail.com>
2023-11-15 13:47:19 -0600

    DOM NOV 15th

---
+++ js/attributes-script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+(function() {
+    document.addEventListener("DOMContentLoaded", function() {
+        const profilePic = document.getElementById("profile-pic");
+
+        setTimeout(function() {
+            profilePic.src = "img/pexels-pia-b-3046629.jpg";
+        }, 2000);
+    });
+    document.addEventListener("DOMContentLoaded", function() {
+        const profileName = document.getElementById("profile-name");
+
+
+        setTimeout(function() {
+            profileName.textContent = "DOC OCT";
+        }, 4000);
+
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const profcolor = document.getElementById('profile-desc');
+
+        setTimeout(function () {
+            profcolor.style.color = 'blue';
+        }, 6000);
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const togglercol = document.getElementById('toggle-btn');
+        const colorsRun = ["red", "blue", "orange", "yellow", "purple", "green", "purple-violet", "black", "olive", "grey",];
+        let colorIndx = 0;
+
+        const toggBackground = () => {
+            togglercol.style.backgroundColor = colorsRun[colorIndx];
+            colorIndx = (colorIndx + 1) % colorsRun.length;
+        }
+
+        setInterval(function () {
+            toggBackground(); // Invoke the function to set the background color
+        }, 2000);
+    });
+
+
+})();
